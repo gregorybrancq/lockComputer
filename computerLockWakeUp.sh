48a3699861a41a4838dbcaab34934601373e7869
Gregory Brancq <gregory.brancq@free.fr>
2020-03-21 10:38:52 +0100

    Corrected link path
    Change-Id: I4083cec6587d029ab5af55362a359fd91261a137

--- computerLockWakeUp.sh
+++ computerLockWakeUp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # this script must be linked here by root user
-# ln -s /home/greg/Greg/work/env/projects/computerLock/computerLockWakeUp.sh /lib/systemd/system-sleep/computerLockWakeUp
+# ln -s /home/greg/Greg/work/env/computerLock/computerLockWakeUp.sh /lib/systemd/system-sleep/computerLockWakeUp
 
 export DISPLAY=:0.0
 export XAUTHORITY=/home/greg/.Xauthority
