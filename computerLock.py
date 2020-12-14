d31e1b18c44de615fa1ea1ca4f834b6fd8c595d2
Gregory Brancq <gregory.brancq@free.fr>
2020-12-14 18:36:10 +0100

    Use workdays configuration

--- computerLock.py
+++ computerLock.py
@@ -25,26 +25,26 @@ runningBackupFile = os.path.join("/tmp/backupNight.running")
 # True will suspend pc
 
 # Holidays
-userSlot = [
-    ["lundi", {"00:30": True, "06:00": False}],
-    ["mardi", {"00:30": True, "06:00": False}],
-    ["mercredi", {"00:30": True, "06:00": False}],
-    ["jeudi", {"00:30": True, "06:00": False}],
-    ["vendredi", {"00:30": True, "06:00": False}],
-    ["samedi", {"02:00": True, "06:00": False}],
-    ["dimanche", {"02:00": True, "06:00": False}]
-]
+# userSlot = [
+#     ["lundi", {"00:30": True, "06:00": False}],
+#     ["mardi", {"00:30": True, "06:00": False}],
+#     ["mercredi", {"00:30": True, "06:00": False}],
+#     ["jeudi", {"00:30": True, "06:00": False}],
+#     ["vendredi", {"00:30": True, "06:00": False}],
+#     ["samedi", {"02:00": True, "06:00": False}],
+#     ["dimanche", {"02:00": True, "06:00": False}]
+# ]
 
 # Work week
-# userSlot = [
-#     ["lundi",    {"00:00": True, "06:00": False, "23:45": True}],
-#     ["mardi",    {"00:00": True, "06:00": False, "23:45": True}],
-#     ["mercredi", {"00:00": True, "06:00": False, "23:45": True}],
-#     ["jeudi",    {"00:00": True, "06:00": False, "23:45": True}],
-#     ["vendredi", {"00:00": True, "06:00": False}],
-#     ["samedi",   {"00:00": False, "02:00": True, "07:00": False}],
-#     ["dimanche", {"00:00": False, "02:00": True, "07:00": False, "23:45": True}]
-# ]
+userSlot = [
+    ["lundi",    {"00:00": True, "06:00": False, "23:45": True}],
+    ["mardi",    {"00:00": True, "06:00": False, "23:45": True}],
+    ["mercredi", {"00:00": True, "06:00": False, "23:45": True}],
+    ["jeudi",    {"00:00": True, "06:00": False, "23:45": True}],
+    ["vendredi", {"00:00": True, "06:00": False}],
+    ["samedi",   {"00:00": False, "02:00": True, "07:00": False}],
+    ["dimanche", {"00:00": False, "02:00": True, "07:00": False, "23:45": True}]
+]
 
 
 ##############################################
